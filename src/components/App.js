1d45ebf44cc239e5902c82685ba473ca4096dfbb
Michael Mathias <michaelscodingjourney@gmail.com>
2023-06-01 15:44:34 -0700

    Passed seven tests for react hooks state and events lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isOn, setIsOn] = useState(false);
+  const appClass = isOn ? "App dark" : "App light";
+  const colorMode = isOn ? "Light Mode" : "Dark Mode";
+  
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{colorMode}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
